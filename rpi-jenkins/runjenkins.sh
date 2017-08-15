f5371a0d9a606b6aa64b3f2872f97e77ad171d12
Peter LaValle <peter.lavalle@gmail.com>
2017-08-15 14:14:23 +0000

    old changes

--- rpi-jenkins/runjenkins.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run --name jenkinsdata -v /root/.jenkins_home:/var/jenkins_home resin/rpi-raspbian:jessie bash
-docker run --restart=always -d -p 8080:8080 -v $(which docker):/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock --volumes-from jenkinsdata -v /lib/arm-linux-gnueabihf/libudev.so.1:/lib/arm-linux-gnueabihf/libudev.so.1  -v /lib/arm-linux-gnueabihf/libdevmapper.so.1.02.1:/lib/arm-linux-gnueabihf/libdevmapper.so.1.02.1 dilgerm/rpi-jenkins
